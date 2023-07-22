22850178b7884dfb5bdbbd6f6100f982104934dc
Ивайло Шопов <is41062491@edu.mon.bg>
2023-07-23 00:49:59 +0300

    Add project files.

---
+++ ParkingSystem/Controllers/CarController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParkingSystem.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ParkingSystem.Controllers
+{
+    public class CarController : Controller
+    {
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            Match match = Regex.Match(car.PlateNumber, @"^(?<region>[A-Z]{1,2})(?:[A-Z0-9]{6})$");
+            if (car.CarMake.Length>0 && car.PlateNumber.Length>0 && match.Success)
+            {
+                DataAccess.Cars.Add(car);
+            }
+                return Redirect("/");
+            
+        }
+        [HttpPost]
+        public IActionResult DeleteCar(string plateNumber)
+        {
+            Car car = DataAccess.Cars.FirstOrDefault(x => x.PlateNumber == plateNumber);
+            DataAccess.Cars.Remove(car);
+            return Redirect("/");
+        }
+    }
+}
---
+++ ParkingSystem/Data/Car.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParkingSystem.Data
+{
+    public class Car
+    {
+        [Required]
+        public string CarMake { get; set; }
+        [Required]
+        public string PlateNumber { get; set; }
+    }
+}
